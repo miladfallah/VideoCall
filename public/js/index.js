76cf7bec297090caf0544556e1f17d596d644e17
Milad Fallah <falahmilad79@gmail.com>
2022-02-10 14:35:24 +0330

    front side of socket added to show active-user-list.

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,28 @@ const { RTCPeerConnection, RTCSessionConnection } = window;
 
 const peerConnection = new RTCPeerConnection();
 
+socket.on("update-user-list", ( { users }) => {
+    const activeUserContainer = document.getElementById("active-user-container");
+
+    users.forEach( (socketId) => {
+        const userExist = document.getElementById(socketId);
+
+        if(!userExist) {
+            const userContainer = document.createElement("div");
+            const username = document.createElement("p");
+
+            userContainer.setAttribute("class", "active-user");
+            userContainer.setAttribute("id", socketId);
+
+            username.setAttribute("class", "username");
+            username.innerHTML = `کاربر: ${socketId}`;
+
+            userContainer.appendChild(username);
+            activeUserContainer.appendChild(userContainer);
+        }
+    });
+});
+
 navigator.getUserMedia( {video: true, audio: true}, (stream) => {
     const localVideo = document.getElementById("local-video");
     if (localVideo) {
