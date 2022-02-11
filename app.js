3bbc1ce2fbc8c3697f7c48fe815f5bb21ac4b6ce
Milad Fallah <falahmilad79@gmail.com>
2022-02-12 01:02:37 +0330

    webRTC added.

--- app.js
+++ app.js
@@ -26,6 +26,25 @@ io.on("connection", (socket) => {
         });
         socket.broadcast.emit("update-user-list", { users: [socket.id] });
     }
+    socket.on("call-user", data => {
+        socket.to(data.to).emit("call-made", {
+            offer: data.offer,
+            socket: socket.id,
+        });
+    });
+
+    socket.on("make-answer", data => {
+        socket.to(data.to).emit("answer-made", {
+            socket: socket.id,
+            answer: data.answer
+        });
+    });
+
+    socket.on("reject-call", data => {
+        socket.to(data.from).emit("call-rejected", {
+            socket: socket.id});
+    });
+
 io.on("disconnect", () => {
     activeUsers = activeUsers.filter(
         (socketExist) => socketExist !== socket.id
--- public/js/index.js
+++ public/js/index.js
@@ -1,17 +1,31 @@
-const socket = io();
+const socket = io("localhost:3000");
 
-const { RTCPeerConnection, RTCSessionConnection } = window;
+let isAlreadyCalling = false;
+let getCalled = false;
+
+const { RTCPeerConnection, RTCSessionDescription } = window;
 
 const peerConnection = new RTCPeerConnection();
 
-socket.on("update-user-list", ( { users }) => {
-    const activeUserContainer = document.getElementById("active-user-container");
+async function callUser(socketId) {
+    const offer = await peerConnection.createOffer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(offer));
+    socket.emit("call-user", {
+        offer,
+        to: socketId
+    });
+} 
 
-    users.forEach( (socketId) => {
-        const userExist = document.getElementById(socketId);
+function unselectedUser() {
+    const alreadySelectedUser = document.querySelectorAll(".active-user.active-user--selected");
 
-        if(!userExist) {
-            const userContainer = document.createElement("div");
+    alreadySelectedUser.forEach(element => {
+        element.setAttribute("class", "active-user");
+    });
+}
+
+function createUserItems(socketId) {
+    const userContainer = document.createElement("div");
             const username = document.createElement("p");
 
             userContainer.setAttribute("class", "active-user");
@@ -21,16 +35,93 @@ socket.on("update-user-list", ( { users }) => {
             username.innerHTML = `کاربر: ${socketId}`;
 
             userContainer.appendChild(username);
-            activeUserContainer.appendChild(userContainer);
+            userContainer.addEventListener("click", () => {
+                unselectedUser();
+                userContainer.setAttribute(
+                    "class",
+                    "active-user active-user--selected");
+                const talkingWithInfo =
+                    document.getElementById("talking-with-info");
+                talkingWithInfo.innerHTML = `تماس با: ${socketId}`;
+                callUser(socketId);
+            });
+            return userContainer;
+        }
+
+function updateUserList(users) {
+    const activeUserContainer = document.getElementById("active-user-container");
+
+    users.forEach( (socketId) => {
+        const userExist = document.getElementById(socketId);
+
+        if(!userExist) {
+            const userContainer = createUserItems(socketId);
+            activeUserContainer.appendChild(userContainer);           
         }
     });
+}
+
+socket.on("update-user-list", ( { users }) => {
+   updateUserList(users);
 });
+socket.on("remove-user", ( {socketId }) => {
+    const user= document.getElementById(socketId);
+    if(user) {
+        user.remove();
+    }
+});
+
+socket.on("call-made", async (data) => {
+    if(getCalled){
+    const confirmed = confirm(`تماس از طرف ${data.socket} 
+    آیا قبول میکنید؟`);
+    if (!confirmed) {
+        //Reject
+        socket.emit("reject-call", {
+            from: data.socket
+        });
+        return;
+    }
+}
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.offer));
+    const answer = await peerConnection.createAnswer();
+    await peerConnection.setLocalDescription(new RTCSessionDescription(answer));
+    socket.emit("make-answer", {
+        answer,
+        to: data.socket,
+    });
+    getCalled = true;
+});
+
+socket.on("answer-made", async(data) => {
+    await peerConnection.setRemoteDescription(new RTCSessionDescription(data.answer));
+
+    if(!isAlreadyCalling){
+    callUser(data.socket);
+    isAlreadyCalling = true;
+    }
+});
+
+
+socket.on("call-rejected", (data) => {
+    alert(` کاربر ${data.socket} تماس شما را رد کرد! `);
+    unselectedUser();
+});
+
+peerConnection.ontrack = function ( {streams: [stream] }) {
+    const remoteVideo = document.getElementById("remote-video");
+    if (remoteVideo) {
+        remoteVideo.srcObject = stream;
+    }
+}
 
 navigator.getUserMedia( {video: true, audio: true}, (stream) => {
     const localVideo = document.getElementById("local-video");
     if (localVideo) {
         localVideo.srcObject = stream;
     }
+    stream.getTracks().forEach((track) => 
+        peerConnection.addTrack(track, stream));
 },
 (error) => {
     console.log(error.message);
